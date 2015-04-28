727c52ee10a176e9797c827b9823e4a2176bbd19
sidestepism <fushimi.ryohei@gmail.com>
2015-04-28 14:23:17 +0900

    Initial Commit

---
+++ improc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  improc
+//
+//  Created by Ryohei Fushimi on 2015/04/28.
+//  Copyright (c) 2015年 Ryohei Fushimi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
