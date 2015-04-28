70c7d1f5dccf2ca71f97ece2a0ec816b0aa4e7bd
sidestepism <fushimi.ryohei@gmail.com>
2015-04-28 14:33:42 +0900

    グレイスケールフィルタ

--- improc/main.cpp
+++ improc/main.cpp
@@ -7,9 +7,22 @@
 //
 
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    cv::Mat src_img = cv::imread("/Users/ryohei/gitrepos/improc/improc/lenna.jpg", 1);
+    if(src_img.empty()) return -1;
+    cv::namedWindow("image1", CV_WINDOW_AUTOSIZE|CV_WINDOW_FREERATIO);
+    cv::Mat proc_img = cv::Mat();
+    
+    cv::cvtColor(src_img, proc_img, CV_RGB2GRAY);
+    for(int i = 0; i < ;i ++){
+        for(int j = 0; j <; j++){
+            
+        }
+    }
+    cv::imshow("image1", proc_img);
+    cv::waitKey(0);
     return 0;
 }
